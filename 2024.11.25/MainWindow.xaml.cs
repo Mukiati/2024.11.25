711ee402fc4efb1bf0525ceca53d603d91511854
Bertók Attila <bertok.attila.21i@egressy.info>
2024-11-25 14:31:33 +0100

    Add project files.

---
+++ 2024.11.25/MainWindow.xaml.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Net.Http;
+using Newtonsoft.Json;
+
+namespace _2024._11._25
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        async void Addapple(object s, EventArgs e)
+        {
+            HttpClient client = new HttpClient();
+            string url = "http://127.1.1.1:4444/alma";
+            try
+            {
+                var jsonObject = new
+                {
+                    type = nev.Text,
+                    price = ar.Text
+                };
+
+                string jsonData = JsonConvert.SerializeObject(jsonObject);
+                StringContent data = new StringContent(jsonData, Encoding.UTF8, "application/json");
+
+                HttpResponseMessage response = await client.PostAsync(url, data);
+                response.EnsureSuccessStatusCode();
+            }
+            catch (Exception error)
+            {
+
+                MessageBox.Show(error.Message);
+            }
+
+           
+
+           
+            //MessageBox.Show($"Alma neve: {nev.Text} , Alma ára: {ar.Text}");
+        }
+    }
+}
